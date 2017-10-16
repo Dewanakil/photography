889de0aaa105066b02c96651ddc366a4e7e1ed59
Dewanakil <akilgazi2.ioe@gmail.com>
2017-10-16 21:12:18 +0600

    full site complited

---
+++ assets/js/style.js
@@ -0,0 +1,32 @@
+//Gallery
+$(function(){
+  $('#MixItUp1').mixItUp({
+    selectors: {
+      filter: '.filter-1',
+      sort: '.sort-1'
+    }
+  });
+});
+//partner
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
